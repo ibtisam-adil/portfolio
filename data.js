103b4be34f905098a1d917eabba92863e3a62f7f
ibtisam-adil <ibtisamadil273@gmail.com>
2024-02-28 09:11:37 +0500

    popup

--- data.js
+++ data.js
@@ -1,16 +1,38 @@
-
-
-let sectionCard = document.querySelector('.cards');
+let sectionCard = document.querySelector(".cards");
 
 let arr = [
-    { img: "./assets/images/poppic.svg", name: "Tonic", desc: "A daily selection of privately personalized reads; no accounts or sign-ups required.", work: ['CANOPY', 'Back End Dev', '2015'], lang: ['html', 'css', 'javaScript', 'Ruby on rails']},
-    { img: "./assets/images/poppic.svg", name: "Multi-Post Stories", desc: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.", work: ['FACEBOOK', 'Full Stack Dev', '2015'], lang: ['html', 'css', 'javaScript', 'Ruby on rails']},
-    { img: "./assets/images/poppic.svg", name: "Facebook 360", desc: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.", work: ['FACEBOOK', 'Full Stack Dev', '2015'], lang: ['html', 'css', 'javaScript', 'Ruby on rails']},
-    { img: "./assets/images/poppic.svg", name: "Uber Navigation", desc: "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.", work: ['UBER', 'Lead Developer', '2018'], lang: ['html', 'css', 'javaScript', 'Ruby on rails']}
+  {
+    img: "./assets/images/poppic.svg",
+    name: "Tonic",
+    desc: "A daily selection of privately personalized reads; no accounts or sign-ups required.",
+    work: ["CANOPY", "Back End Dev", "2015"],
+    lang: ["html", "css", "javaScript", "Ruby on rails"],
+  },
+  {
+    img: "./assets/images/poppic.svg",
+    name: "Multi-Post Stories",
+    desc: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
+    work: ["FACEBOOK", "Full Stack Dev", "2015"],
+    lang: ["html", "css", "javaScript", "Ruby on rails"],
+  },
+  {
+    img: "./assets/images/poppic.svg",
+    name: "Facebook 360",
+    desc: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.",
+    work: ["FACEBOOK", "Full Stack Dev", "2015"],
+    lang: ["html", "css", "javaScript", "Ruby on rails"],
+  },
+  {
+    img: "./assets/images/poppic.svg",
+    name: "Uber Navigation",
+    desc: "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
+    work: ["UBER", "Lead Developer", "2018"],
+    lang: ["html", "css", "javaScript", "Ruby on rails"],
+  },
 ];
 
-let cardHTMLs = arr.map((val)=>{
-    return `
+let cardHTMLs = arr.map((val) => {
+  return `
         <div class="card-one">
             <div class="card-img">
                 <img src="${val.img}"/>
@@ -40,14 +62,14 @@ let cardHTMLs = arr.map((val)=>{
     `;
 });
 
-sectionCard.innerHTML = cardHTMLs.join('');
+sectionCard.innerHTML = cardHTMLs.join("");
 
-let popup = document.getElementById("popup");
-
-function openPopup(){
+function openPopup(event) {
+    let popup = document.getElementById("popup");
     popup.classList.add("open-popup");
+
 }
 
-function closePopup(){
+function closePopup() {
     popup.classList.remove("open-popup");
 }
