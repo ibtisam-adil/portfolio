3b15a1c7acccc2e9514aef9817485dbde8009b23
ibtisam-adil <ibtisamadil273@gmail.com>
2024-04-29 09:00:30 +0500

    Used real life data

--- array.js
+++ array.js
@@ -2,48 +2,44 @@
 
 let projects = [
     { 
-        img: "./assets/images/poppic.svg", 
-        name: "Tonic", 
-        desc: "A daily selection of privately personalized reads; no accounts or sign-ups required.", 
-        work: ['CANOPY', 'Back End Dev', '2015'], 
-        lang: ['html', 'css', 'javaScript', 'Ruby on rails'], 
-        detailedDesc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.` 
+        img: "./assets/images/bookstore.png", 
+        name: "BookStore CMS",
+        desc: "A React web app for managing book collections, enabling users to view and control their books.", 
+        work: ['Self-employed', 'Front End Dev', '2024'], 
+        lang: ['JavaScript', 'React', 'Redux'], 
+        detailedDesc: `The Bookstore project is a dynamic React web application featuring smooth navigation with React Router V6. It utilizes Redux for state management and fetches book data from an API, allowing users to seamlessly explore categorized books, track reading progress, and manage their personalized book lists.`,
+        liveURL: "https://ibtisam-bookstore.netlify.app/",
+        sourceURL: "https://github.com/ibtisam-adil/bookstore.git"
     },
     { 
-        img: "./assets/images/poppic.svg", 
-        name: "Multi-Post Stories", 
-        desc: "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.", 
-        work: ['FACEBOOK', 'Full Stack Dev', '2015'], 
-        lang: ['html', 'css', 'javaScript', 'Ruby on rails'], 
-        detailedDesc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.` 
+        img: "./assets/images/calc.png", 
+        name: "Math Magician", 
+        desc: "A versatile calculator app for efficient math operations with welcoming and calculator web pages. ", 
+        work: ['Self-employed', 'Front End Dev', '2024'], 
+        lang: ['html', 'javaScript', 'React'], 
+        detailedDesc: `This project builds upon the core functionality of the previously developed Calculator component to create a comprehensive website consisting of three distinct pages: Home, Calculator, and Quote. The primary aim is to demonstrate proficiency in React development, including routing and page integration.`,
+        liveURL: "https://ibtisam-calculator-app.netlify.app",
+        sourceURL: "https://github.com/ibtisam-adil/Math-Magician.git"
     },
     { 
-        img: "./assets/images/poppic.svg", 
-        name: "Facebook 360", 
-        desc: "Exploring the future of media in Facebook's first Virtual Reality app; a place to discover and enjoy 360 photos and videos on Gear VR.", 
-        work: ['FACEBOOK', 'Full Stack Dev', '2015'], 
-        lang: ['html', 'css', 'javaScript', 'Ruby on rails'], 
-        detailedDesc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.` 
+        img: "./assets/images/spacetravela.png", 
+        name: "Space Traveler's Hub", 
+        desc: "React and Redux web app utilizing SpaceX API for rocket and mission info, reservations, and profiles.", 
+        work: ['Self-employed', 'Front End Dev', '2024'], 
+        lang: ['JavaScript', 'React', 'Redux'], 
+        detailedDesc: `Welcome to the Space Travelers' Hub, a web application designed for booking rockets, joining space missions, and exploring the wonders of space travel. Developed as a React & Redux group project, this application serves as a comprehensive learning experience while delivering practical functionalities.`,
+        liveURL: "https://spacetravellers-hub.netlify.app",
+        sourceURL: "https://github.com/ibtisam-adil/SpaceTraveler.git"
     },
     { 
-        img: "./assets/images/poppic.svg", 
+        img: "./assets/images/pokemon.png", 
         name: "Uber Navigation", 
-        desc: "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.", 
-        work: ['UBER', 'Lead Developer', '2018'], 
-        lang: ['html', 'css', 'javaScript', 'Ruby on rails'], 
-        detailedDesc: `Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s, Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
-        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it 1960s.` 
+        desc: "Explore Pokémon in a dynamic web app with intuitive navigation, liking, and commenting features.", 
+        work: ['Self-employed', 'Lead Developer', '2024'], 
+        lang: ['html', 'css', 'javaScript'], 
+        detailedDesc: `"Pokemon" is a JavaScript web app that brings Pokémon to life with a dynamic interface and intuitive navigation. Users choose from rock, water, fire, and lightning Pokémon types, floating around a Pokeball. Leveraging an external API, the app fetches Pokémon data, enabling exploration and interaction through liking and commenting features. Implemented with classes, these features seamlessly integrate with the API for a smooth user experience.`,
+        liveURL: " https://pokemondiscovery.netlify.app",
+        sourceURL: "https://github.com/ibtisam-adil/pokemon.git" 
     }
 ];
 
